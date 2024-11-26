340da04fc564ffdacacc520d89435fd733daae76
Ken Tseng <tsen1220.us@gmail.com>
2024-11-27 03:01:55 +0800

    feat():add logger tests.

---
+++ logger/logger_test.go
@@ -0,0 +1,69 @@
+package logger
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogrusLogger_Info(t *testing.T) {
+	// 設置測試鉤子來捕獲日誌
+	logger := NewLogrusLogger()
+	hook := test.NewGlobal()
+	logger.(*LogrusLogger).logger.AddHook(hook)
+
+	// 呼叫 Info 方法
+	logger.Info("This is an info message")
+
+	// 檢查 Info 日誌是否存在
+	assert.Len(t, hook.Entries, 1)
+	assert.Equal(t, logrus.InfoLevel, hook.Entries[0].Level)
+	assert.Equal(t, "This is an info message", hook.Entries[0].Message)
+}
+
+func TestLogrusLogger_Warn(t *testing.T) {
+	// 設置測試鉤子來捕獲日誌
+	logger := NewLogrusLogger()
+	hook := test.NewGlobal()
+	logger.(*LogrusLogger).logger.AddHook(hook)
+
+	// 呼叫 Warn 方法
+	logger.Warn("This is a warn message")
+
+	// 檢查 Warn 日誌是否存在
+	assert.Len(t, hook.Entries, 1)
+	assert.Equal(t, logrus.WarnLevel, hook.Entries[0].Level)
+	assert.Equal(t, "This is a warn message", hook.Entries[0].Message)
+}
+
+func TestLogrusLogger_Error(t *testing.T) {
+	// 設置測試鉤子來捕獲日誌
+	logger := NewLogrusLogger()
+	hook := test.NewGlobal()
+	logger.(*LogrusLogger).logger.AddHook(hook)
+
+	// 呼叫 Error 方法
+	logger.Error("This is an error message")
+
+	// 檢查 Error 日誌是否存在
+	assert.Len(t, hook.Entries, 1)
+	assert.Equal(t, logrus.ErrorLevel, hook.Entries[0].Level)
+	assert.Equal(t, "This is an error message", hook.Entries[0].Message)
+}
+
+func TestLogrusLogger_Debug(t *testing.T) {
+	// 設置測試鉤子來捕獲日誌
+	logger := NewLogrusLogger()
+	hook := test.NewGlobal()
+	logger.(*LogrusLogger).logger.AddHook(hook)
+
+	// 呼叫 Debug 方法
+	logger.Debug("This is a debug message")
+
+	// 檢查 Debug 日誌是否存在
+	assert.Len(t, hook.Entries, 1)
+	assert.Equal(t, logrus.DebugLevel, hook.Entries[0].Level)
+	assert.Equal(t, "This is a debug message", hook.Entries[0].Message)
+}
